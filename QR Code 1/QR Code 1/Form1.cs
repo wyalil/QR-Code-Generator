6ac9d1a1230e05e12f31e0e7c29d5c702e65896e
w0x <121750106+wyalil@users.noreply.github.com>
2023-01-01 13:34:08 +0300

    Add files via upload
    Kaynak Kodlu QR Code oluşturucu

---
+++ QR Code 1/QR Code 1/Form1.cs
@@ -0,0 +1,22 @@
+using QRCoder;
+
+namespace QR_Code_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = tbMetin.Text;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, "#0000ff", "#FFFFFF");
+            pictureBox1.Image = qrCodeImage;
+        }
+    }
+}
